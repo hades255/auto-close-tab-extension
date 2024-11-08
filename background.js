ace47ffa86cdd53f0c09be6a3a212658cb040fd6
hades255 <hades.255@outlook.com>
2024-11-09 00:42:49 +0900

    version1

--- background.js
+++ background.js
@@ -1,19 +1,37 @@
+let CLOSE_TIME = 11;
+let SITE_URL = "upwork.com";
+
+const checkTime = () => {
+  const now = new Date();
+  const newYorkTime = new Intl.DateTimeFormat("en-US", {
+    timeZone: "America/New_York",
+    hour: "2-digit",
+    hour12: false,
+  }).format(now);
+
+  return newYorkTime === CLOSE_TIME.toString();
+};
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.url && tab.url.includes("devdocs.io")) {
-    setTimeout(() => {
-      chrome.tabs.remove(tabId);
-    }, 60000); // Close the tab after 60 seconds
+  if (changeInfo.url && tab.url.includes(SITE_URL)) {
+    const timerFunc = () => {
+      setTimeout(timerFunc, 60000);
+      if (checkTime()) chrome.tabs.remove(tabId);
+    };
+    timerFunc();
   }
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'setTimer') {
-      const url = message.url;
-      chrome.tabs.create({ url: url }, (newTab) => {
-        setTimeout(() => {
-          chrome.tabs.remove(newTab.id);
-        }, 60000); // Close the tab after 60 seconds
-      });
-    }
-  });
-  
+  if (message.action === "setTimer") {
+    const time = message.time;
+    if (time) CLOSE_TIME = time;
+    const url = message.url;
+    if (url) SITE_URL = url;
+    // chrome.tabs.create({ url: url }, (newTab) => {
+    //   setTimeout(() => {
+    //     chrome.tabs.remove(newTab.id);
+    //   }, 60000); // Close the tab after 60 seconds
+    // });
+  }
+});
--- popup.js
+++ popup.js
@@ -1,4 +1,12 @@
 document.getElementById("closeButton").addEventListener("click", () => {
   const url = document.getElementById("url").value;
-  chrome.runtime.sendMessage({ action: "setTimer", url: url });
+  const time = document.getElementById("time").value;
+  chrome.runtime.sendMessage({ action: "setTimer", url, time });
+});
+
+document.getElementById("openButton").addEventListener("click", () => {
+  const url = document.getElementById("url").value;
+  chrome.tabs.create({ url: url }, (newTab) => {
+    // chrome.tabs.remove(newTab.id);
+  });
 });
